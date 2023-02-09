31214dc0ffbe32500f2c23e1991b3677c9f5d61e
emspa07 <empaulin.dwfsc14@bootcamp.ucamp.io>
2023-02-09 16:14:30 -0600

    cree el archivo main js

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let description = document.getElementById ('description')
+let goal = document.getElementById('goal')
+let day = document.getElementById('day')
+
+let btnSubmit = document.getElementById('btnSubmit')
+let btnReset = document.getElementById('btnReset')
+
+let formJournal = document.getElementById('formJournal')

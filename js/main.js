ae23e37d4671592dab1f042f17aee3388eab2846
emspa07 <empaulin.dwfsc14@bootcamp.ucamp.io>
2023-02-09 22:51:14 -0600

    Pruebas en localStorage

--- js/main.js
+++ js/main.js
@@ -1,8 +1,69 @@
-let description = document.getElementById ('description')
-let goal = document.getElementById('goal')
-let day = document.getElementById('day')
+let descriptionInput = document.getElementById ('description')
+let goalInput = document.getElementById('goal')
+let dayInput = document.getElementById('day')
 
-let btnSubmit = document.getElementById('btnSubmit')
-let btnReset = document.getElementById('btnReset')
+let btnAdd = document.getElementById('btnAdd')
 
 let formJournal = document.getElementById('formJournal')
+let entriesDiv = document.getElementById('entries')
+
+document.addEventListener('DOMContentLoaded', () => {
+    let tasks = JSON.parse(localStorage.getItem('tasks'))
+    
+    if (tasks === null) {
+        let paragraph = document.createElement("p")
+        let textParagraph = document.createTextNode("No hay entradas en tu journal")
+        
+        paragraph.appendChild(textParagraph)
+        entriesDiv.append(paragraph)
+    } else {
+        for (let i = 0; i <= tasks.length; i++){
+            let taskDiv = document.createElement("div")
+            let textDescriptionGoal = document.createTextNode(`${tasks[i].description}-${tasks[i].goal} `)
+            
+            let buttonDelete = document.createElement('button')
+            let textButtonDelete = document.createTextNode('Eliminar')
+            buttonDelete.appendChild(textButtonDelete)
+
+            taskDiv.appendChild(textDescriptionGoal)
+            taskDiv.appendChild(buttonDelete)
+
+            entriesDiv.appendChild(taskDiv)
+        }
+    }
+
+    btnAdd.addEventListener('click', (e) => {
+        let tasks = JSON.parse(localStorage.getItem('tasks')) || []
+
+        let description = descriptionInput.value
+        let goal = goalInput.value
+        let day = dayInput.value
+
+        let task = {
+            "description": description,
+            "goal": goal,
+            "day": day
+        }
+        console.log(task)
+
+        tasks.push(task)
+
+        console.log(tasks)
+
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+
+        for (let i = 0; i <= tasks.length; i++){
+            let taskDiv = document.createElement("div")
+            let textDescriptionGoal = document.createTextNode(`${tasks[i].description}-${tasks[i].goal} `)
+            
+            let buttonDelete = document.createElement('button')
+            let textButtonDelete = document.createTextNode('Eliminar')
+            buttonDelete.appendChild(textButtonDelete)
+
+            taskDiv.appendChild(textDescriptionGoal)
+            taskDiv.appendChild(buttonDelete)
+
+            entriesDiv.appendChild(taskDiv)
+        }
+    })
+})
